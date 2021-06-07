c6435ed02eccca951f1412be6e987c0d112366f5
josephdsbr@gmail.com <josephdsbr@gmail.com>
2021-06-06 23:42:31 -0300

    adicionando o layer dos componentes

---
+++ src/app/components/component.module.ts
@@ -0,0 +1,7 @@
+import { UserCardModule } from './user-card/user-card.module';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [UserCardModule],
+})
+export class ComponentsModule {}
